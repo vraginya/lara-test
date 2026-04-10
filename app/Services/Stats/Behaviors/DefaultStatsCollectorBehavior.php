09a24daa7c374c7be5719d5cd3931923c84bf4f3
valery <v.raginya@gmail.com>
2026-04-10 11:41:17 +0200

    New commit

--- app/Services/Stats/Behaviors/DefaultStatsCollectorBehavior.php
+++ app/Services/Stats/Behaviors/DefaultStatsCollectorBehavior.php
@@ -2,9 +2,11 @@
 
 namespace App\Services\Stats\Behaviors;
 
+use App\Models\Appointment;
+
 class DefaultStatsCollectorBehavior extends AbstractStatsCollectorBehavior
 {
-    public function doCollect(): void
+    public function doCollect(Appointment $appointment): void
     {
         // Implement the default statistics collection logic here
         // For example, you could collect general statistics about appointments, users, etc.
